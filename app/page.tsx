c0ceee1dc593a9515f90005db264db9eae679ed5
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-06-08 11:29:51 -0500

    added header bg and h1

--- app/page.tsx
+++ app/page.tsx
@@ -25,6 +25,16 @@ export default function Home() {
         </div>
       </nav>
       {/* NAVBAR */}
+      <main>
+        {/* HEADER */}
+        <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
+          <div className="text-center mt-10">
+          <h1 className="text-white text-5xl font-bold mb-2">Find your table for any occassion</h1>
+          </div>
+
+        </div>
+         {/* HEADER */}
+      </main>
     </main>
   );
 }
