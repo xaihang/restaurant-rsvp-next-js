8f21c72e494bc9887a6b4b1c186eabc655ff8f34
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-06-08 11:23:38 -0500

    added nav btns and bg color styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,30 @@
 // import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Hello Next World!</h1>
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className="max-w-screen-2xl m-auto bg-white">HELLO NEXT WORLD</main>
+      {/* NAVBAR */}
+      <nav className="bg-white p-2 flex justify-between">
+        <a href="" className="font-bold text-gray-700 text-2xl">
+          OpenTable
+        </a>
+        <div>
+          <div className="flex">
+            <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
+              Sign in
+            </button>
+            <button className="text-black border p-1 px-4 rounded">
+              Sign up
+            </button>
+          </div>
+        </div>
+      </nav>
+      {/* NAVBAR */}
     </main>
-  )
+  );
 }
